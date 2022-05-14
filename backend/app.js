9f8572e0ebc8319b3e805a3f510f734f4e510e47
Yannick <Yannmarsot@hotmail.fr>
2022-05-14 13:10:26 +0200

    Ajout mongoose et modéle "User"

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://Yannick:pNhXS19Y8326LHvi@cluster0.fkzmn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res) => {
   res.json({ message: "Votre requête a bien été reçue !" });
