0c287994d9a4556a8af2c8dc0d3f4ebc26641631
Yannick <Yannmarsot@hotmail.fr>
2022-05-21 16:35:42 +0200

    ajout fichier controllers + routes
    installation "bcrypt" + "jsonwebtoken"

---
+++ backend/controllers/user.js
@@ -0,0 +1,7 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+exports.signup = (req, res, next) => {};
+
+exports.login = (req, res, next) => {};
