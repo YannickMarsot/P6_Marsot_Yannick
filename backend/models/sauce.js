b3bd8f3ca32d8511625ab41bb6e8bdad069d1d7e
Yannick <Yannmarsot@hotmail.fr>
2022-05-21 20:32:01 +0200

    ajout modèle sauce

---
+++ backend/models/sauce.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const sauceShema = mongoose.Schema({
+  //creation d'un modéle desauce
+  userId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  //comment faire pour implanter un tableau d'user?
+  usersLiked: { type: String, required: true },
+  usersDisliked: { type: String, required: true },
+});
+
+sauceShema.plugin(uniqueValidator);
+module.exports = mongoose.model("Sauce", sauceShema);
