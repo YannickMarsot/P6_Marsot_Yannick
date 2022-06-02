91b653ef5794ffb2fcea8439786312409839b9d7
Yannick <Yannmarsot@hotmail.fr>
2022-06-02 18:00:38 +0200

    nouveaux protocol cors

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.json());
 const helmet = require("helmet");
 require("dotenv").config();
-app.use(helmet());
+const cors = require("cors");
+
 const path = require("path");
 
 const userRoutes = require("./routes/user");
@@ -21,22 +22,22 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use((req, res, next) => {
-  //"en-tête" des requêtes
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+// app.use((_req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 
+app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/auth", userRoutes);
 //app.use("/api/sauces", sauceRoutes);
-
+app.use(helmet());
 module.exports = app;
