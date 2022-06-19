bbdef8f535e7fe4b2b7f101ad210e7172970dba4
Yannick Marsot <yannmarsot@hotmail.fr>
2022-06-19 13:47:08 +0200

    mise en place routes "sauces"

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const path = require("path");
 
 const userRoutes = require("./routes/user");
-//const sauceRoutes = require("./routes/sauce");
+const sauceRoutes = require("./routes/sauce");
 
 mongoose
   //connection à mongoDb
@@ -22,22 +22,9 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-// app.use((_req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
-
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/auth", userRoutes);
-//app.use("/api/sauces", sauceRoutes);
+app.use("/api/sauces", sauceRoutes);
 app.use(helmet());
 module.exports = app;
